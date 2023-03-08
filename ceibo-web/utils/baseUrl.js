0f26728dd6b5b52f3cf15a8700cb5f3b94cbc52c
Sergio Milla <sergiomilla2019@gmail.com>
2023-03-08 07:51:55 -0300

    cambio url

--- ceibo-web/utils/baseUrl.js
+++ ceibo-web/utils/baseUrl.js
@@ -1,7 +1,7 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://001-ceibo-web-front-bc71.vercel.app"
-    : // ? "https://ceibo.digital"
-      "http://localhost:3000"
+    ? "https://ceibo.digital"
+    : // ? "https://001-ceibo-web-front-bc71.vercel.app"
+      "https://localhost:3000"
 
 export default baseUrl
