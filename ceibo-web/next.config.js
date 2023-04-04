c7242e909b08cd1151304ef64c259fecc0f63c9b
Sergio Milla <sergiomilla2019@gmail.com>
2023-04-04 15:17:32 -0300

    Add security headers

--- ceibo-web/next.config.js
+++ ceibo-web/next.config.js
@@ -3,7 +3,50 @@
 const nextTranslate = require("next-translate")
 const path = require("path")
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src example.com;
+  style-src 'self' example.com;
+  font-src 'self'; ` 
+
+let securityheaders = [{
+  key: 'X-DNS-Prefetch-Control',
+  value: 'on'
+},
+{
+  key: 'Strict-Transport-Security',
+  value: 'max-age=63072000; includeSubDomains; preload'
+},
+{
+  key: 'X-XSS-Protection',
+  value: '1; mode=block'
+},
+{
+  key: 'X-Frame-Options',
+  value: 'SAMEORIGIN'
+},
+{
+  key: 'X-Content-Type-Options',
+  value: 'nosniff'
+},
+{
+  key: 'Referrer-Policy',
+  value: 'origin-when-cross-origin'
+},
+{
+  key: 'Content-Security-Policy',
+  value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+}];
+
 const nextConfig = {
+  async headers(){
+    return[{
+      source: "/:path*",
+      headers: securityheaders,
+    }
+  ]
+  },
   async redirects() {
     return [
       {
