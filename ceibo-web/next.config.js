b569fac094c68f36a52b9454963029716b8f09bb
Lucio FLores <lucio.e.flores@gmail.com>
2023-04-04 16:34:18 -0300

    restore next.config.js

--- ceibo-web/next.config.js
+++ ceibo-web/next.config.js
@@ -1,69 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const nextTranslate = require("next-translate")
-const path = require("path")
-
-const ContentSecurityPolicy = `default-src *;` 
-
-let securityheaders = [{
-  key: 'X-DNS-Prefetch-Control',
-  value: 'on'
-},
-{
-  key: 'Strict-Transport-Security',
-  value: 'max-age=63072000; includeSubDomains; preload'
-},
-{
-  key: 'X-XSS-Protection',
-  value: '1; mode=block'
-},
-{
-  key: 'X-Frame-Options',
-  value: 'SAMEORIGIN'
-},
-{
-  key: 'X-Content-Type-Options',
-  value: 'nosniff'
-},
-{
-  key: 'Referrer-Policy',
-  value: 'no-referrer, strict-origin-when-cross-origin'
-},
-];
-
-//{
-//  key: 'Content-Security-Policy',
-//  value: ContentSecurityPolicy
-//}
-
+const nextTranslate = require("next-translate");
+const path = require("path");
 
 const nextConfig = {
-  async headers(){
-    return[{
-      source: "/:path*",
-      headers: securityheaders,
-    }
-  ]
-  },
-  async redirects() {
-    return [
-      {
-        source: "/empleos/:empleo*",
-        destination: "/sumate-al-equipo",
-        permanent: true,
-      },
-      {
-        source: "/tag/:tagg*",
-        destination: "/publicaciones",
-        permanent: true,
-      },
-      {
-        source: "/category/:categori*",
-        destination: "/publicaciones",
-        permanent: true,
-      },
-    ]
-  },
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: true,
@@ -85,14 +25,14 @@ const nextConfig = {
     defaultLocale: "es-ES",
     // This is a list of locale domains and the default locale they
     // should handle (these are only required when setting up domain routing)
-    // Note: subdomains must be included in the domain value to be matched e.g. "fr.ceibo.digital localhost:3000".
+    // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
     domains: [
       {
-        domain: "ceibo.digital",
+        domain: "example.com",
         defaultLocale: "es-ES",
       },
       {
-        domain: "ceibo.digital/en",
+        domain: "example.com/en",
         defaultLocale: "en-US",
         // an optional http field can also be used to test
         // locale domains locally with http instead of https
@@ -100,8 +40,8 @@ const nextConfig = {
       },
     ],
   },
-}
+};
 
-module.exports = nextTranslate(nextConfig)
+module.exports = nextTranslate(nextConfig);
 
 // module.exports = nextConfig
