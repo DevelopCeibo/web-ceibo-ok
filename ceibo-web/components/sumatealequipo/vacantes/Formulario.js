4498ea56a47ac5156864e9aaf990da0ae39b1cbf
matiob <buraschi.mateo@gmail.com>
2023-06-09 17:01:31 -0300

    add required logic to checboxs in vacantes formulario

--- ceibo-web/components/sumatealequipo/vacantes/Formulario.js
+++ ceibo-web/components/sumatealequipo/vacantes/Formulario.js
@@ -8,16 +8,16 @@ import {
   Checkbox,
   FormControlLabel,
   TextField,
-} from "@mui/material"
-import { Box } from "@mui/system"
-import React, { useState } from "react"
-import axios from "axios"
-import Swal from "sweetalert2"
-import withReactContent from "sweetalert2-react-content"
-const MySwal = withReactContent(Swal)
-import baseUrl from "../../../utils/baseUrl"
-import useTranslation from "next-translate/useTranslation"
-import { useForm } from "react-hook-form"
+} from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useState } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+const MySwal = withReactContent(Swal);
+import baseUrl from "../../../utils/baseUrl";
+import useTranslation from "next-translate/useTranslation";
+import { useForm } from "react-hook-form";
 
 const alertContent = () => {
   MySwal.fire({
@@ -27,8 +27,8 @@ const alertContent = () => {
     timer: 2000,
     timerProgressBar: true,
     showConfirmButton: false,
-  })
-}
+  });
+};
 const alertError = () => {
   MySwal.fire({
     icon: "error",
@@ -36,8 +36,8 @@ const alertError = () => {
     timer: 3000,
     text: "Algo salió mal!",
     footer: "Podes intentarlo más tarde, o comunicarte por otro medio",
-  })
-}
+  });
+};
 // Form initial state
 const INITIAL_STATE = {
   name: "",
@@ -46,52 +46,49 @@ const INITIAL_STATE = {
   subject: "",
   lkdurl: "",
   cv: "",
-}
+};
 
 const Formulario = ({ vacante }) => {
-  const { t } = useTranslation("common")
-  const completaform = t("completaform")
-  const nombreyape = t("nombreyape")
-  const telefono = t("telefono")
-  const comoteEnteraste = t("comoteEnteraste")
-  const bolsatrabajo = t("bolsatrabajo")
-  const feriauni = t("feriauni")
-  const ceiboweb = t("ceiboweb")
-  const recomendacion = t("recomendacion")
-  const instaAd = t("instaAd")
-  const instaPost = t("instaPost")
-  const otros = t("otros")
-  const adjCv = t("adjCv")
-  const enviar = t("enviar")
+  const { t } = useTranslation("common");
+  const completaform = t("completaform");
+  const nombreyape = t("nombreyape");
+  const telefono = t("telefono");
+  const comoteEnteraste = t("comoteEnteraste");
+  const bolsatrabajo = t("bolsatrabajo");
+  const feriauni = t("feriauni");
+  const ceiboweb = t("ceiboweb");
+  const recomendacion = t("recomendacion");
+  const LinkedIn = t("linkedin");
+  const instaPost = t("instaPost");
+  const otros = t("otros");
+  const adjCv = t("adjCv");
+  const enviar = t("enviar");
 
-  const nombre = t("nombre")
-
-  const [checked, setchecked] = useState([])
+  const [checked, setchecked] = useState([]);
 
   const handleCheckbox = (e) => {
-    const FormControlLabel = e.target.parentNode.parentNode
-    const labelContent = FormControlLabel.lastElementChild.textContent
-
+    const FormControlLabel = e.target.parentNode.parentNode;
+    const labelContent = FormControlLabel.lastElementChild.textContent;
     if (checked.includes(labelContent)) {
-      let index = checked.indexOf(labelContent)
+      let index = checked.indexOf(labelContent);
       setchecked((prevArray) =>
         prevArray.slice(0, index).concat(prevArray.slice(index + 1))
-      )
+      );
     } else {
-      setchecked((prev) => [...prev, labelContent])
+      setchecked((prev) => [...prev, labelContent]);
     }
-  }
+  };
 
-  const [contact, setContact] = useState(INITIAL_STATE)
+  const [contact, setContact] = useState(INITIAL_STATE);
 
   const handleChange = (e) => {
-    const { name, value } = e.target
+    const { name, value } = e.target;
     if (name == "cv") {
-      setContact((prevState) => ({ ...prevState, [name]: e.target.files[0] }))
+      setContact((prevState) => ({ ...prevState, [name]: e.target.files[0] }));
     } else {
-      setContact((prevState) => ({ ...prevState, [name]: value }))
+      setContact((prevState) => ({ ...prevState, [name]: value }));
     }
-  }
+  };
 
   const {
     register,
@@ -113,45 +110,47 @@ const Formulario = ({ vacante }) => {
     //   required: "Este campo es requerido",
     //   pattern: "El formato del correo electrónico no es válido",
     // },
-  })
+  });
 
   const onSubmit = async (data) => {
     // e.preventDefault()
-    const { email, name, number, lkdurl } = data
+    const { email, name, number, lkdurl } = data;
     try {
-      const url = `${baseUrl}/api/contact`
-      const { cv } = contact
+      const url = `${baseUrl}/api/contact`;
+      const { cv } = contact;
       // const { name, email, number, lkdurl, cv } = contact
-      const formData = new FormData()
-      formData.append("name", name)
-      formData.append("email", email)
-      formData.append("number", number)
-      formData.append("lkdurl", lkdurl)
-      formData.append("checked", checked)
-      formData.append("subject", "Nueva postulación a vacante en Ceibo")
-      formData.append("type", "recruiting")
-      formData.append("vacante", vacante)
-      formData.append("cv", cv)
+      const formData = new FormData();
+      formData.append("name", name);
+      formData.append("email", email);
+      formData.append("number", number);
+      formData.append("lkdurl", lkdurl);
+      formData.append("checked", checked);
+      formData.append("subject", "Nueva postulación a vacante en Ceibo");
+      formData.append("type", "recruiting");
+      formData.append("vacante", vacante);
+      formData.append("cv", cv);
 
-      const response = await axios.post(url, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      })
-
-      console.log(response.data)
-      if (response.status == 200) {
-        setContact(INITIAL_STATE)
-        setchecked([])
-        alertContent()
-      } else {
-        alertError()
+      if (checked.length) {
+        const response = await axios.post(url, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+  
+        console.log(response.data);
+        if (response.status == 200) {
+          setContact(INITIAL_STATE);
+          setchecked([]);
+          alertContent();
+        } else {
+          alertError();
+        }
       }
     } catch (error) {
-      console.log(error)
-      alertError()
+      console.log(error);
+      alertError();
     }
-  }
+  };
 
   return (
     <>
@@ -267,9 +266,9 @@ const Formulario = ({ vacante }) => {
                       onChange={handleCheckbox}
                     />
                   }
-                  label="LinkedIn"
+                  label={LinkedIn}
                 ></FormControlLabel>
-                <FormControlLabel
+                {/*                 <FormControlLabel
                   control={
                     <Checkbox
                       checked={checked.includes(instaAd)}
@@ -277,7 +276,7 @@ const Formulario = ({ vacante }) => {
                     />
                   }
                   label={instaAd}
-                ></FormControlLabel>
+                ></FormControlLabel> */}
                 <FormControlLabel
                   control={
                     <Checkbox
@@ -296,6 +295,11 @@ const Formulario = ({ vacante }) => {
                   }
                   label={otros}
                 ></FormControlLabel>
+                {!checked.length && (
+                  <Typography variant="caption" color="error">
+                    This field is required.
+                  </Typography>
+                )}
               </FormGroup>
 
               <Typography component="h6" sx={{ my: 2 }}>
@@ -310,7 +314,7 @@ const Formulario = ({ vacante }) => {
                 required
                 onChange={handleChange}
                 {...register("cv", {
-                  required: "This field is required"
+                  required: "This field is required",
                 })}
               />
 
@@ -325,7 +329,7 @@ const Formulario = ({ vacante }) => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Formulario
+export default Formulario;
