3e8e6227515ae95ad3fbee644adb1fd811afd1ea
matiob <buraschi.mateo@gmail.com>
2023-07-06 23:01:05 -0300

    insight & publicaciones json, blogGrid prop & insights route

---
+++ ceibo-web/pages/insights/[insight].js
@@ -0,0 +1,52 @@
+import React from "react"
+import NavbarTwo from "../../components/Layouts/NavbarTwo"
+import Footer from "../../components/Layouts/Footer"
+import BlogDetailsContent from "../../components/Blog/BlogDetailsContent"
+import insights from "../../utils/insights.json"
+import { useRouter } from "next/router"
+import PageBanner from "../../components/Common/PageBanner"
+import Head from "next/head"
+
+const Insight = () => {
+  const router = useRouter()
+  const { locale } = useRouter()
+  const pathInsight = router.asPath.split("/")[2]
+
+  const [data, setdata] = React.useState({})
+
+  React.useEffect(() => {
+    insights.forEach((insight) => {
+      if (insight["path-url"] === pathInsight) {
+        setdata(insight)
+      }
+    })
+  }, [pathInsight])
+
+  const TITLE_TAG = `${data?.title} - Ceibo Digital`
+  const META_DESC = `${data?.desc}`
+  return (
+    <>
+      <Head>
+        <title>{TITLE_TAG}</title>
+        <meta name="description" content={META_DESC} />
+      </Head>
+      <NavbarTwo />
+      <PageBanner
+        pageTitle={data.title}
+        subtitle={`${
+          locale == "es" ? data?.metadata?.date : data?.metadata?.dateEN
+        }  ${
+          data.metadata?.fuenteOriginal
+            ? "- " + data.metadata?.fuenteOriginal
+            : ""
+        }`}
+        BGImage={data.metadata?.bannerImg}
+        textcenter={true}
+      />
+      <BlogDetailsContent publicacion={data} />
+      <Footer />
+    </>
+  )
+}
+
+export default Insight
--- ceibo-web/pages/insights/index.js
+++ ceibo-web/pages/insights/index.js
@@ -7,33 +7,33 @@ import BlogGridThree from "../../components/Blog/BlogGridThree"
 import Head from "next/head"
 import useTranslation from "next-translate/useTranslation"
 
-const Publicaciones = () => {
+const Insights = () => {
   const { t } = useTranslation("common")
-  const publicacionesh1 = t("publicacionesh1")
-  const publicacionesh2 = t("publicacionesh2")
-  const publicacionesTitleTag = t("publicacionesTitleTag")
-  const publicacionesMetaDesc = t("publicacionesMetaDesc")
+  const insightsh1 = t("insightsh1")
+  const insightsh2 = t("insightsh2")
+  const insightsTitleTag = t("insightsTitleTag")
+  const insightsMetaDesc = t("insightsMetaDesc")
 
   return (
     <>
       <Head>
-        <title>{publicacionesTitleTag}</title>
-        <meta name="description" content={publicacionesMetaDesc} />{" "}
+        <title>{insightsTitleTag}</title>
+        <meta name="description" content={insightsMetaDesc} />{" "}
       </Head>
       <NavbarTwo />
       <PageBanner
-        pageTitle={publicacionesh1}
-        subtitle={publicacionesh2}
+        pageTitle={insightsh1}
+        subtitle={insightsh2}
         BGImage={"/images-ceibo/Banner-publicaciones.jpg"}
         // bgcolor="#b72837"
       />
       <div className="bg-fcfbfb pt-100 pb-70 casos-de-exito">
         {/* <div className="container ">asd</div> */}
-        <BlogGridThree section={'publicaciones'}/>
+        <BlogGridThree section={'insights'}/>
       </div>
       <Footer />
     </>
   )
 }
 
-export default Publicaciones
+export default Insights
