009b01c50e020cdc918a28b2cdf5ffbef2cdb1fb
tomiapo <tomasapochian@gmail.com>
2023-08-10 12:43:48 -0300

    adjust baseUrl

--- ceibo-web/utils/baseUrl.js
+++ ceibo-web/utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://ceibo.digital"
+    ? "https://www.ceibo.digital"
     : // ? "https://web-ceibo-ok-git-develop-ceibodigital.vercel.app/"
       "http://localhost:3000";
 
