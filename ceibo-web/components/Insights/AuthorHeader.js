6aff6d2acceab5b1b9e24319dfadc6de9ff9dbb1
tomiapo <tomasapochian@gmail.com>
2023-08-11 13:47:10 -0300

    add styles

--- ceibo-web/components/Insights/AuthorHeader.js
+++ ceibo-web/components/Insights/AuthorHeader.js
@@ -1,17 +1,32 @@
 import React from "react";
 
 function AuthorHeader({ author, date }) {
+  const authorStyles = {
+    color: "grey",
+    paddingBottom: "1%",
+    fontSize: "20px",
+  };
   return (
     <div className="insight-data">
       {author?.length === 1 ? (
         <h2 className="insight-author">{`Por ${author}`}</h2>
       ) : (
-        <div className='insight-data-authors'>
-        {author?.map((author, i) => {
-            return i === 0
-            ? (<h2 className="insight-author" key={i}>{`Por ${author}`}</h2>)
-            : (<h2 className="insight-author" key={i}>{`y ${author}`}</h2>)
-        })}
+        <div className="insight-data-authors">
+          {author?.map((author, i) => {
+            return i === 0 ? (
+              <h2
+                className="insight-author"
+                style={authorStyles}
+                key={i}
+              >{`Por ${author}`}</h2>
+            ) : (
+              <h2
+                className="insight-author"
+                key={i}
+                style={authorStyles}
+              >{`y ${author}`}</h2>
+            );
+          })}
         </div>
       )}
       {date && <h2 className="insight-date">{date}</h2>}
