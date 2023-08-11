fbb84a2561ed3b19ebcab613dc4f0b3855c6b8cd
tomiapo <tomasapochian@gmail.com>
2023-08-11 13:48:21 -0300

    fix linkedin button

--- ceibo-web/components/Insights/DownloadFileButton.js
+++ ceibo-web/components/Insights/DownloadFileButton.js
@@ -2,15 +2,22 @@ import React from "react";
 import Link from "next/link";
 import useTranslation from "next-translate/useTranslation";
 
-function DownloadFileButton({link}) {
+function DownloadFileButton({ link }) {
   const { t } = useTranslation("common");
   const download = t("fullDownload");
+  const downloadNote = t("fullDownloadNote");
   const pdfLink = link;
   return (
     <div className="download-button">
       <div className="container btn-two-container">
         <Link href={`#`}>
-          <a className="default-btn-two anchor-download" href={pdfLink} target={"_blank"}>{download}</a>
+          <a
+            className="default-btn-two anchor-download"
+            href={pdfLink}
+            target={"_blank"}
+          >
+            {download}
+          </a>
         </Link>
       </div>
     </div>
