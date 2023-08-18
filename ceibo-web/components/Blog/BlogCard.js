e4a529ba816691bbf0cd63a40f7051078fd60284
tomiapo <tomasapochian@gmail.com>
2023-08-18 16:02:30 -0300

    transform date to uppercase

--- ceibo-web/components/Blog/BlogCard.js
+++ ceibo-web/components/Blog/BlogCard.js
@@ -13,7 +13,7 @@ function BlogCard({ section, publi }) {
       {section === "insights" ? (
         // INSIGHTS
         <div className="col-lg-4 col-md-6">
-          <div className="single-blog-item ">
+          <div className="single-blog-item">
             <div className="blog-image">
               <Link href={`/${urlFragment}/${publi["path-url"]}`}>
                 <a>
@@ -22,24 +22,28 @@ function BlogCard({ section, publi }) {
               </Link>
             </div>
             <div
-              className={`blog-post-content ${locale == "es" ? "blog" : null} insight-card`}
+              className={`blog-post-content ${
+                locale == "es" ? "blog" : null
+              } insight-card`}
             >
               <h3>
-                <a className='insight-core'>{locale == "en" 
-                  ? `${publi.themeEN} ${publi?.articleEN}` 
-                  : `${publi.theme} ${publi?.article}`}</a>
+                <a className="insight-core">
+                  {locale == "en"
+                    ? `${publi.themeEN} ${publi?.articleEN}`
+                    : `${publi.theme} ${publi?.article}`}
+                </a>
               </h3>
               <span className="date">
                 {locale == "es"
-                  ? publi?.metadata?.date
-                  : publi?.metadata?.dateEN}
+                  ? publi?.metadata?.date.toUpperCase()
+                  : publi?.metadata?.dateEN.toUpperCase()}
               </span>
-              <h3 className='insight-card-title'>
+              <h3 className="insight-card-title">
                 <Link href={`/${urlFragment}/${publi["path-url"]}`}>
                   <a>{locale == "en" ? publi.titleEN : publi.title}</a>
                 </Link>
               </h3>
-              <p className='desc-insight'>
+              <p className="desc-insight">
                 {locale == "es"
                   ? publi.desc
                   : "*This article is written in Spanish"}
@@ -68,7 +72,9 @@ function BlogCard({ section, publi }) {
               className={`blog-post-content ${locale == "es" ? "blog" : null}`}
             >
               <span className="date">
-                {locale == "es" ? publi?.metadata.date : publi?.metadata.dateEN}
+                {locale == "es"
+                  ? publi?.metadata.date.toUpperCase()
+                  : publi?.metadata.dateEN.toUpperCase()}
               </span>
               <h3>
                 <Link href={`/${urlFragment}/${publi["path-url"]}`}>
