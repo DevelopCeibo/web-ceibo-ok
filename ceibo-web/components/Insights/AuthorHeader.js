0a66e46c28b1ae3f9d2e119fdae36194802c42ab
MaleBurs <maleburs@gmail.com>
2023-08-18 10:41:07 -0300

    authors issue

--- ceibo-web/components/Insights/AuthorHeader.js
+++ ceibo-web/components/Insights/AuthorHeader.js
@@ -6,32 +6,25 @@ function AuthorHeader({ author, date }) {
     paddingBottom: "1%",
     fontSize: "20px",
   };
+  
   return (
     <div className="insight-data">
-      {author?.length === 1 ? (
-        <h2
-          className="insight-author"
-          style={authorStyles}
-        >{`Por ${author}`}</h2>
-      ) : (
-        <div className="insight-data-authors">
-          {author?.map((author, i) => {
-            return i === 0 ? (
-              <h2
-                className="insight-author"
-                style={authorStyles}
-                key={i}
-              >{`Por ${author}`}</h2>
-            ) : (
-              <h2
-                className="insight-author"
-                key={i}
-                style={authorStyles}
-              >{`y ${author}`}</h2>
-            );
-          })}
+        <div className="insight-data-authors"> 
+          <h2
+            className="insight-author"
+            style={authorStyles}
+          >
+            {author?.map((anAuthor, i) => {
+              return i === 0 ? (
+                `Por ${anAuthor}`
+              ) : (
+                i=== author.length-1 ? 
+                ` y ${anAuthor}` :
+                `, ${anAuthor}`
+              );
+            })}       
+          </h2>            
         </div>
-      )}
       {date && (
         <h2 className="insight-date" style={authorStyles}>
           {date}
