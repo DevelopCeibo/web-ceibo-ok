046ab04e5d420e279b19a591d341c056f20c57a5
tomiapo <tomasapochian@gmail.com>
2023-09-04 19:39:40 -0300

    add  job search categorised component

---
+++ ceibo-web/components/sumatealequipo/BusquedasVigentesCategorizado.js
@@ -0,0 +1,41 @@
+import React from "react";
+import useTranslation from "next-translate/useTranslation";
+import vacantes from "../../utils/vacantes.json";
+import BusquedasVigentesIndividual from "./BusquedasVigentesIndividual";
+
+const BusquedasVigentes = () => {
+  const { t } = useTranslation("common");
+  const rolesParaConsultores = t("rolesParaConsultores");
+  const rolesParaEspecialistas = t("rolesParaEspecialistas");
+
+  const vacantesConsultores = vacantes.filter(
+    (vac) => vac.roleType === "consultant"
+  );
+  const vacantesEspecialistas = vacantes.filter(
+    (vac) => vac.roleType === "specialist"
+  );
+
+  return (
+    <div className="pt-70 pb-100 mb-3 mt-4">
+      <div className="container">
+        <div className="section-title " id="busquedas-vigentes">
+          <h2>{rolesParaConsultores}</h2>
+        </div>
+
+        <BusquedasVigentesIndividual arregloDeVacantes={vacantesConsultores} />
+
+        <div className="container mt-6">
+          <div className="section-title " id="busquedas-vigentes">
+            <h2>{rolesParaEspecialistas}</h2>
+          </div>
+
+          <BusquedasVigentesIndividual
+            arregloDeVacantes={vacantesEspecialistas}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BusquedasVigentes;
