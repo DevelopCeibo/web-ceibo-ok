049a5a72dffb3fe665935f3d8eeb0eaaa1839214
tomiapo <tomasapochian@gmail.com>
2023-09-11 20:54:58 -0300

    create cardWithVideo commons

--- ceibo-web/components/Common/CardWithVideo.js
+++ ceibo-web/components/Common/CardWithVideo.js
@@ -4,17 +4,16 @@ const CardWithVideo = ({ videoURL, title, text }) => {
   return (
     <div className="card-with-video background-grey">
       <iframe
-        className="card-video"
-        width="560"
-        height="315"
         src={videoURL}
         title={title}
         frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen="allowfullscreen"
+        allow="accelerometer; autoplay;clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen="allowfullscreen"
       ></iframe>
-      <h5>{title} </h5>
-      <p>{text}</p>
+      <div className="card-with-video-content">
+        <h5>{title} </h5>
+        <p>{text}</p>
+      </div>
     </div>
   );
 };
