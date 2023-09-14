20999a3595ceb31ca937fa827ed3a1becaaa0501
tomiapo <tomasapochian@gmail.com>
2023-09-14 11:22:20 -0300

    create desarrollo-profesional section and components

---
+++ ceibo-web/components/sumatealequipo/desarrollo-profesional/CarreraEnCeibo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CarreraEnCeibo = () => {
+  return <div>CarreraEnCeibo</div>;
+};
+
+export default CarreraEnCeibo;
--- ceibo-web/pages/sumate-al-equipo/desarrollo-profesional/index.js
+++ ceibo-web/pages/sumate-al-equipo/desarrollo-profesional/index.js
@@ -1,10 +1,34 @@
 import React from "react";
+import Head from "next/head";
+import NavbarFive from "../../../components/Layouts/NavbarFive";
+
+import PageBanner from "../../../components/Common/PageBanner";
+import Footer from "../../../components/Layouts/Footer";
+import CarreraEnCeibo from "../../../components/sumatealequipo/desarrollo-profesional/CarreraEnCeibo";
+import Especializaciones from "../../../components/sumatealequipo/desarrollo-profesional/Especializaciones";
+import ComoNosOrganizamos from "../../../components/Cultura/ComoNosOrganizamos";
+import NavigationCardSection from "../../../components/sumatealequipo/posiciones-abiertas/NavigationCardSection";
 
 const index = () => {
   return (
-    <div>
-      <h1>WIP</h1>
-    </div>
+    <>
+      <Head>
+        <title>{"sumateTitleTag"}</title>
+        <meta name="description" content={"sumateMetaDesc"} />
+      </Head>
+      <NavbarFive />
+      <PageBanner
+        pageTitle={"sumateh1"}
+        subtitle={"sumateh2"}
+        BGImage="/images-ceibo/Banner-sumate-al-equipo.jpg"
+      />
+
+      <CarreraEnCeibo />
+      <Especializaciones />
+      <ComoNosOrganizamos />
+      <NavigationCardSection />
+      <Footer />
+    </>
   );
 };
 
