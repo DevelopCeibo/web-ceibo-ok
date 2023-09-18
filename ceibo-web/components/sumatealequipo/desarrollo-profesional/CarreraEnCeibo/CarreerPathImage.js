477d727d5cfecd1830e44621656951a92ee1df61
tomiapo <tomasapochian@gmail.com>
2023-09-18 19:59:28 -0300

    create Career path Image component

---
+++ ceibo-web/components/sumatealequipo/desarrollo-profesional/CarreraEnCeibo/CarreerPathImage.js
@@ -0,0 +1,41 @@
+import React from "react";
+import useTranslation from "next-translate/useTranslation";
+import { useRouter } from "next/router";
+import Image from "next/image";
+const CarreerPathImage = () => {
+  const { t } = useTranslation("common");
+  const { locale } = useRouter();
+  return (
+    <>
+      <div className="d-none d-md-block d-lg-block">
+        <Image
+          src={`/images-ceibo/sumatealequipo/${
+            locale == "es"
+              ? "carrera-en-ceibo-español.jpg"
+              : "Career-Path-inglés_1-11.png"
+          }`}
+          alt="carrera en ceibo"
+          width={5}
+          height={3}
+          layout="responsive"
+        />
+      </div>
+      {/*mobile screen image */}
+      <div className="d-md-none d-lg-none">
+        <Image
+          src={`/images-ceibo/sumatealequipo/${
+            locale == "es"
+              ? "career-path-español-mobile.png"
+              : "Career-Path-inglés_1-11.png"
+          }`}
+          alt="carrera en ceibo"
+          width={5}
+          height={5}
+          layout="responsive"
+        />
+      </div>
+    </>
+  );
+};
+
+export default CarreerPathImage;
