774deb5a80f546bd5581ea1f9948d10ec4ec595e
matiob <buraschi.mateo@gmail.com>
2023-09-18 11:55:27 -0300

    especiaizaciones component add locales text data

--- ceibo-web/components/sumatealequipo/desarrollo-profesional/Especializaciones.js
+++ ceibo-web/components/sumatealequipo/desarrollo-profesional/Especializaciones.js
@@ -4,17 +4,17 @@ import useTranslation from "next-translate/useTranslation";
 
 const Especializaciones = () => {
   const { t } = useTranslation("common");
-  const espacializacionTitle = 'Especializaciones' // t("espacializacionTitle");
-  const especializacionText = 'En Ceibo, creemos en forjar un camino profesional único y lleno de posibilidades para cada colaborador. Y es por eso que los consultores tienen la oportunidad de especializarse en tres verticales distintas: datos, growth marketing y desarrollo. Estas especializaciones se pueden dar a través de capacitaciones específicas, experiencia práctica en proyectos relacionados, y trainings tanto dentro de la empresa como en colaboración con instituciones académica. La oportunidad de especializarse permite a los Ceibers fortalecer sus habilidades técnicas, adquirir experiencia práctica y convertirse en líderes en el campo de especialización elegido.'
-  const dataTitle = 'Datos'
-  const dataText = 'En un mundo impulsando por la información, nuestros Especialistas en Datos son los encargados de extraer de ella significado y valor. Su dominio de herramientas y técnicas analíticas les permite explorar conjuntos de datos con precisión, identificando patrones y tendencias que sustentan decisiones estratégicas.'
-  const growthTitle = "Growth Marketing"
-  const growthText = 'Nuestros expertos en Growth Marketing ayudan a impulsar el crecimiento de nuestros clientes  mediante estrategias de marketing efectivas. No solo entienden las métricas, sino que también saben cómo traducirlas en estrategias innovadoras y experimentos que generan un impacto medible y escalable.'
-  const developerTitle = 'Desarrollo'
-  const developerText = 'Nuestros especialistas en desarrollo son los creadores de las aplicaciones, sistemas y programas que transforman ideas en soluciones concretas. Combinando su ingenio creativo con habilidades técnicas sólidas, estos expertos trabajan en la intersección entre la creatividad y la funcionalidad, asegurando que cada línea de código sea una puerta abierta hacia experiencias digitales intuitivas y fluidas.'
+  const espacializacionTitle = t("espacializacionTitle");
+  const especializacionText = t("especializacionText");
+  const dataTitle = t("dataTitle");
+  const dataText = t("dataText");
+  const growthTitle = t("growthTitle");
+  const growthText = t("growthText");
+  const developerTitle = t("developerTitle");
+  const developerText = t("developerText");
   return (
     <section className="pt-70 mb-5">
-      {/* TITULO */}
+      {/* TITLE */}
       <div className="section-title esp-title">
         <h2> {espacializacionTitle}</h2>
         <p className="esp-subtitle esp-responsive">{especializacionText}</p>
