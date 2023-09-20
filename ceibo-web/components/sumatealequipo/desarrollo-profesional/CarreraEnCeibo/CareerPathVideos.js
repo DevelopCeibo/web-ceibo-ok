31e324e1fbd7f88bed1e5f81d135220b6d9d7a3d
tomiapo <tomasapochian@gmail.com>
2023-09-20 14:47:41 -0300

    remove padding top

--- ceibo-web/components/sumatealequipo/desarrollo-profesional/CarreraEnCeibo/CareerPathVideos.js
+++ ceibo-web/components/sumatealequipo/desarrollo-profesional/CarreraEnCeibo/CareerPathVideos.js
@@ -11,7 +11,7 @@ const CareerPathVideos = () => {
   );
   console.log(videoContentArray);
   return (
-    <div className="row mt-5 pt-4">
+    <div className="row mt-5">
       {videoContentArray?.map((content) => {
         return (
           <div
