16a8ba0b1f64ad027f802a4fb3fbae1a1694b525
tomiapo <tomasapochian@gmail.com>
2023-09-26 16:35:46 -0300

    create index component

---
+++ ceibo-web/components/sumatealequipo/proceso-de-reclutamiento/ComoPrepararse/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import ThreeColumns from "./ThreeColumns";
+import ColumnWithVideo from "./ColumnWithVideo";
+const ComoPrepararse = () => {
+  return (
+    <div className="container">
+      <ColumnWithVideo />
+      <ThreeColumns />
+    </div>
+  );
+};
+
+export default ComoPrepararse;
