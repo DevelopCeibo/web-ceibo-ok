ad4d0f2137aa1ba8eff5af1acf4e6417b8970827
tomiapo <tomasapochian@gmail.com>
2023-09-26 16:35:33 -0300

    create component

---
+++ ceibo-web/components/sumatealequipo/proceso-de-reclutamiento/ComoPrepararse/ColumnWithVideo.js
@@ -0,0 +1,85 @@
+import React from "react";
+import useTranslation from "next-translate/useTranslation";
+import CardWithVideo from "../../../Common/CardWithVideo";
+import ThreeColumns from "./ThreeColumns";
+const ColumnWithVideo = () => {
+  const { t } = useTranslation("common");
+  const title = t("comoPrepararse");
+  const paragraphs = t("comoPrepararseParagraphs", {}, { returnObjects: true });
+  const paragraphListItems = t(
+    "comoPrepararseItems",
+    {},
+    { returnObjects: true }
+  );
+  const comoPrepararseFinalParagraph = t("comoPrepararseFinalParagraph");
+  const videoContent = t(
+    "comoPrepararseVideoContent",
+    {},
+    { returnObjects: true }
+  );
+  return (
+    <>
+      <div className="section-title">
+        <h2>{title}</h2>
+      </div>
+      <div className="row mb-5">
+        <div className="col-12 col-xl-7 como-prepararse-column-responsive">
+          {paragraphs?.map((p) => {
+            return <p className="text-justify">{p.text}</p>;
+          })}
+          <div className="row justify-content-center">
+            <div className="col-6 col-sm-6 col-md-6">
+              {paragraphListItems?.map((p, index) => {
+                {
+                  return index <= 2 ? (
+                    <div className="row d-flex flex-nowrap ms-1 mb-3 align-items-center">
+                      <div className="col-1 d-flex como-prepararse-list-index">
+                        {index + 1}
+                      </div>
+                      <div className="col-11">
+                        <p>{p.text}</p>
+                      </div>
+                    </div>
+                  ) : (
+                    ""
+                  );
+                }
+              })}
+            </div>
+            <div className="col-6 col-sm-6 col-md-6">
+              {paragraphListItems?.map((p, index) => {
+                {
+                  return index >= 3 ? (
+                    <div className="row d-flex flex-nowrap ms-1 mb-3 align-items-center">
+                      <div className="col-1 d-flex como-prepararse-list-index">
+                        {index + 1}
+                      </div>
+                      <div className="col-11">
+                        <p>{p.text}</p>
+                      </div>
+                    </div>
+                  ) : (
+                    ""
+                  );
+                }
+              })}
+            </div>
+          </div>
+
+          <p className="mb-3 text-justify mt-4 mb-5">
+            {comoPrepararseFinalParagraph}
+          </p>
+        </div>
+        <div className="col-12 col-xl-5 d-flex justify-content-center">
+          <CardWithVideo
+            title={videoContent.title}
+            text={videoContent.description}
+            videoURL={videoContent.videoURL}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ColumnWithVideo;
