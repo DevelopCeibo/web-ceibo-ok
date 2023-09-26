f575d5533914e8c6d8ee3784c95ac404758b6d86
tomiapo <tomasapochian@gmail.com>
2023-09-26 16:36:18 -0300

    adjust import

--- ceibo-web/pages/sumate-al-equipo/proceso-de-reclutamiento/index.js
+++ ceibo-web/pages/sumate-al-equipo/proceso-de-reclutamiento/index.js
@@ -6,14 +6,13 @@ import PageBanner from "../../../components/Common/PageBanner";
 import Footer from "../../../components/Layouts/Footer";
 import PasosParaElProceso from "../../../components/sumatealequipo/PasosParaElProceso";
 import NuestroProceso from "../../../components/sumatealequipo/proceso-de-reclutamiento/NuestroProceso";
-import ComoPrepararse from "../../../components/sumatealequipo/proceso-de-reclutamiento/ComoPrepararse";
+import ComoPrepararse from "../../../components/sumatealequipo/proceso-de-reclutamiento/ComoPrepararse/index";
 import useTranslation from "next-translate/useTranslation";
 const index = () => {
   const { t } = useTranslation("common");
   const h1 = t("carreraEnCeiboH1");
   const h2 = t("carreraEnCeiboH2");
   const metaDescription = t("carreraEnCeiboMetaDescription");
-
 
   return (
     <>
@@ -28,10 +27,9 @@ const index = () => {
         BGImage="/images-ceibo/Banner-sumate-al-equipo.jpg"
       />
 
-      <NuestroProceso/>
-      <PasosParaElProceso/>
-      <ComoPrepararse/>
-
+      <NuestroProceso />
+      <PasosParaElProceso />
+      <ComoPrepararse />
 
       <Footer />
     </>
