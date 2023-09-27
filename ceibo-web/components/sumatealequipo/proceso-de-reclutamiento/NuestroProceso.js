dbce171e05b1d8612eeea65dc3131caed88bb495
matiob <buraschi.mateo@gmail.com>
2023-09-27 20:45:42 -0300

    nuestroProceso component | responsive styles | locales texts

--- ceibo-web/components/sumatealequipo/proceso-de-reclutamiento/NuestroProceso.js
+++ ceibo-web/components/sumatealequipo/proceso-de-reclutamiento/NuestroProceso.js
@@ -9,20 +9,20 @@ const NuestroProceso = () => {
   const trabajoequipo = t("trabajoequipo")
   const desarrollo = t("desarrollo")
 
-  const procesoReclutamientoTitle = "Nuestro Proceso"; // t("procesoReclutamientoTitle");
-  const procesoReclutamientoText =
-    "En CEIBO, nuestro proceso de reclutamiento ha sido diseñado cuidadosamente para garantizar que los candidatos cumplan con todas las expectativas requeridas para nuestras búsquedas y estén alineados con nuestra cultura y valores. Durante este proceso, brindamos a los candidatos la oportunidad de sumergirse en la esencia de CEIBO, interactuando con diversos colaboradores que provienen de distintos perfiles y experiencias.";
-  const procesoReclutamientoEvaluacionTitle = "¿Qué evaluamos?";
-  const procesoReclutamientoEvaluacionText =
-    "Durante todo el proceso nosotros evaluamos 4 dimensiones claves, que son las mismas con las que evaluamos a todos los colaboradores durante su carrera en Ceibo:";
-  const procesoReclutamientoTextFinal = 'Al evaluar estas cuatro dimensiones clave durante el proceso de reclutamiento, podemos identificar a candidatos que poseen la capacidad de resolver problemas complejos, habilidades de comunicación efectivas, la disposición para colaborar en equipos y un deseo constante de desarrollo profesional. Estas cualidades son fundamentales para tener consultores altamente competentes y preparados, capaces de enfrentar los desafíos cambiantes del entorno empresarial y brindar soluciones innovadoras a los clientes.'
+  const procesoReclutamientoTitle = t("procesoReclutamientoTitle");
+  const procesoReclutamientoText = t("procesoReclutamientoText");
+  const procesoReclutamientoEvaluacionTitle = t("procesoReclutamientoEvaluacionTitle");
+  const procesoReclutamientoEvaluacionText = t("procesoReclutamientoEvaluacionText");
+  const procesoReclutamientoTextFinal = t("procesoReclutamientoTextFinal");
 
   return (
     <section className="pt-70 mb-5">
       <section className="container">
+
         <section className="row">
+
+          {/* TITLE */}
           <article className="col-lg-8 col-md-8 col-sm-12">
-            {/* TITLE */}
             <div className="section-title esp-title">
               <h2> {procesoReclutamientoTitle}</h2>
               <p className="recruiting-title-text">
@@ -30,7 +30,8 @@ const NuestroProceso = () => {
               </p>
             </div>
           </article>
-          <article className="col-lg-4 col-md-4 col-sm-12">
+
+          <article className="col-lg-4 col-md-4 col-sm-12 recruiting-responsive">
             <Image
               src="/images-ceibo/sumatealequipo/recruiting/ofertas-de-empleo-en-tecnologia.jpg"
               width={300}
@@ -40,6 +41,7 @@ const NuestroProceso = () => {
             />
           </article>
         </section>
+
         <section className="row pt-3">
           <article className="col-lg-12 col-md-12 col-sm-12">
             <div className="section-title esp-title">
@@ -49,7 +51,9 @@ const NuestroProceso = () => {
               </p>
             </div>
           </article>
-          <section className="row justify-content-center">
+
+          {/* CARDS */}
+          <section className="row justify-content-center recruiting-responsive-cards">
             <article className="col-6 col-sm-4 col-lg-3">
               <div className="ccore-card">
                 <img
@@ -102,9 +106,10 @@ const NuestroProceso = () => {
               </div>
             </article>
           </section>
+
           <article className="row pt-5">
             <div className="section-title esp-title">
-              <p className="recruiting-eva-text recruiting-eva-text-final">
+              <p className="recruiting-eva-text recruiting-eva-text-final recruiting-responsive-text">
                 {procesoReclutamientoTextFinal}
               </p>
             </div>
