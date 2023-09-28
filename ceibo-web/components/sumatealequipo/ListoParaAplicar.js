8a8c2f2fd3ab4d3ebd5a5b91059f9d2f72d8c7cd
tomiapo <tomasapochian@gmail.com>
2023-09-28 09:39:38 -0300

    create component

--- ceibo-web/components/sumatealequipo/ListoParaAplicar.js
+++ ceibo-web/components/sumatealequipo/ListoParaAplicar.js
@@ -1,7 +1,21 @@
 import React from "react";
+import useTranslation from "next-translate/useTranslation";
 
 const ListoParaAplicar = () => {
-  return <div>ListoParaAplicar</div>;
+  const { t } = useTranslation("common");
+  const title = t("listoParaAplicarTitle");
+  const description = t("listoParaAplicarDescription");
+
+  return (
+    <div className="my-5 pb-100">
+      <div className=" d-flex flex-column align-items-center background-maroon listo-para-aplicar">
+        <h2 className="bold text-white my-4">{title}</h2>
+        <a href="/sumate-al-equipo/proceso-de-reclutamiento">
+          <h6 className="bold mb-5 px-4 py-3 text-center">{description}</h6>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default ListoParaAplicar;
--- ceibo-web/components/sumatealequipo/PorQueCeibo.js
+++ ceibo-web/components/sumatealequipo/PorQueCeibo.js
@@ -1,7 +1,33 @@
 import React from "react";
-
+import QueNosHaceDiferentesCard from "../Cultura/QueNosHaceDiferentes/QueNosHaceDiferentesCard";
+import useTranslation from "next-translate/useTranslation";
+import uniqid from "uniqid";
 const PorQueCeibo = () => {
-  return <div>PorQueCeibo</div>;
+  const { t } = useTranslation("common");
+  const cardsContent = t("porQueCeiboCards", {}, { returnObjects: true });
+  const title = t("porQueCeiboDigitalTitle");
+  const description = t("porQueCeiboDigitalParagraph");
+  return (
+    <div className="container mt-5 pt-5">
+      <div className="section-title">
+        <h2 className="mb-5">{title}</h2>
+        <p className="text-center">{description}</p>
+      </div>
+      <div className="row">
+        {cardsContent.map((card) => {
+          return (
+            <div className="col-12 col-md-6 col-lg-3 mt-4" key={uniqid()}>
+              <QueNosHaceDiferentesCard
+                src={card.imagePath}
+                title={card.title}
+                text={card.text}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default PorQueCeibo;
