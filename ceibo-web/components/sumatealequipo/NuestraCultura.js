cd8ccfaea0673c44d088f395a2360f79fdbc49a1
tomiapo <tomasapochian@gmail.com>
2023-09-28 12:27:42 -0300

    adjust padding

--- ceibo-web/components/sumatealequipo/NuestraCultura.js
+++ ceibo-web/components/sumatealequipo/NuestraCultura.js
@@ -10,7 +10,7 @@ const NuestraCultura = () => {
   const knowMore = t("knowMore");
 
   return (
-    <section className="pt-50 mb-5">
+    <section className="pt-50 mb-5 pb-60">
       {/* TITLE */}
       <section className="section-title esp-title">
         <h2> {overviewCulturaTitle}</h2>
@@ -30,7 +30,9 @@ const NuestraCultura = () => {
           </div>
 
           <div className="col-lg-6 col-md-6 col-sm-12 overview-text-wrapper">
-            <p className='overview-cul-text overview-responsive-text'>{overviewCulturaText}</p>
+            <p className="overview-cul-text overview-responsive-text">
+              {overviewCulturaText}
+            </p>
             <div className="container btn-two-container overview-cul-button overview-responsive">
               <Link href="/sumate-al-equipo/vida-en-ceibo">
                 <a className="default-btn-two" title="Cultura en Ceibo">
