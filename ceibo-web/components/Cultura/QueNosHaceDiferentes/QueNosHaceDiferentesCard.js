3c1f7aa845d983be47824623568bd1f840460260
tomiapo <tomasapochian@gmail.com>
2023-09-28 09:39:26 -0300

    adjust component height to maintain 1:1 aspect ratio

--- ceibo-web/components/Cultura/QueNosHaceDiferentes/QueNosHaceDiferentesCard.js
+++ ceibo-web/components/Cultura/QueNosHaceDiferentes/QueNosHaceDiferentesCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const QueNosHaceDiferentesCard = ({ src, title, text }) => {
   return (
     <div className="inst-culture-card">
-      <img src={src} width={120} />
+      <img src={src} width={120} height={120} />
       <h3>{title}</h3>
       <p>{text}</p>
     </div>
