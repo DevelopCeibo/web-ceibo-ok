f4cbf94a1f461792c5ae243beb524fed6aae12a9
Tomas Apochian <tomasapochian@gmail.com>
2023-10-20 11:29:50 -0300

    add navigation section to webinars

--- ceibo-web/pages/eventos/[evento].js
+++ ceibo-web/pages/eventos/[evento].js
@@ -1,29 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavbarFive from "../../components/Layouts/NavbarFive";
 import Footer from "../../components/Layouts/Footer";
-import InsightDetailsContent from "../../components/Insights/InsightDetailsContent";
 import { useRouter } from "next/router";
 import Head from "next/head";
-import LatestNewsSlider from "../../components/Common/LatestNewsSlider";
 import EventBanner from "../../components/Events/EventBanner";
 import events from "../../utils/eventos.json";
 import eventsEN from "../../utils/eventosEN.json";
 import EventPageGenerator from "../../components/Events/EventPageGenerator";
+import EventNavSection from "../../components/Events/EventNavSections";
 
 const Event = () => {
 	const router = useRouter();
 	const { locale } = useRouter();
 	const pathEvent = router.asPath.split("/")[2];
 	const [event, setEvent] = React.useState({});
+	const [eventsToUse, setEventsToUse] = useState([]);
 
-	React.useEffect(() => {
-		let eventsToUse = locale === "es" ? events : eventsEN;
-		eventsToUse.forEach((event) => {
-			if (event["path-url"] === pathEvent) {
-				setEvent(event);
-			}
-		});
-	}, [pathEvent]);
+	useEffect(() => {
+		const selectedEvents = locale === "es" ? events : eventsEN;
+		setEventsToUse(selectedEvents);
+		const selectedEvent = selectedEvents.find(
+			(event) => event["path-url"] === pathEvent
+		);
+		if (selectedEvent) {
+			setEvent(selectedEvent);
+		}
+	}, [pathEvent, locale]);
 
 	const TITLE_TAG = `${event?.title} - Ceibo Digital`;
 	const META_DESC = `${event?.desc}`;
@@ -42,7 +44,7 @@ const Event = () => {
 				date={event?.metadata?.date}
 			/>
 			<EventPageGenerator event={event} />
-			{/* <LatestNewsSlider type={"insights"} /> */}
+			<EventNavSection events={eventsToUse} />
 			<Footer />
 		</>
 	);
