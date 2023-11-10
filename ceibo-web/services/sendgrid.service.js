884bfca06d26c281c6ce2629adb0bbaa7339e030
Tomas Apochian <tomasapochian@gmail.com>
2023-11-10 19:01:46 -0300

    remove console.log

--- ceibo-web/services/sendgrid.service.js
+++ ceibo-web/services/sendgrid.service.js
@@ -6,7 +6,6 @@ export const addEmailToSendgridContactList = async (email) => {
 		const url = `${baseUrl}/api/sendgrid/addContact`;
 		const response = await axios.post(url, { email });
 		const sendgridJobId = response.data;
-		console.log(sendgridJobId, "contact added con exito");
 		return sendgridJobId;
 	} catch (error) {
 		return { error: "Error adding contact to sendgrid database" };
