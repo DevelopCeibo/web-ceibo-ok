2fc647814635e2bff0e49feef17e86343e3ae38a
Tomas Apochian <tomasapochian@gmail.com>
2024-01-03 15:13:06 -0300

    add new 'como nos organizamos section'

--- ceibo-web/components/sumatealequipo/desarrollo-profesional/CarreraEnCeibo/index.js
+++ ceibo-web/components/sumatealequipo/desarrollo-profesional/CarreraEnCeibo/index.js
@@ -3,10 +3,12 @@ import CareerPathImage from "./CareerPathImage";
 import Introduction from "./Introduction";
 import ExploreOurCareerPathSwiper from "./ExploreOurCareerPathSwiper";
 import CareerPathVideos from "./CareerPathVideos";
+import ComoNosOrganizamos from "../../../Cultura/ComoNosOrganizamos";
 
 const Index = () => {
   return (
     <div className="container mt-5 pt-5 mb-5">
+      <ComoNosOrganizamos/>
       <Introduction />
       <CareerPathImage />
       <ExploreOurCareerPathSwiper />
