01406f04357db5ca1df313f6db4e0a6084ffba6b
Tomas Apochian <tomasapochian@gmail.com>
2024-02-16 14:38:36 -0300

    add clients carousel

--- ceibo-web/pages/index.js
+++ ceibo-web/pages/index.js
@@ -33,7 +33,7 @@ const Index = () => {
       <QuienesSomos />
       <Services />
       <ComoAyudamosANuestrosClientes />
-      {/* <Partner /> */}
+      <Partner />
       <Contacto />
       <TrendingInsights />
       {/* <Publicacion /> */}
