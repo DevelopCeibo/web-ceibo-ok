9a4baa7908705cb21cc1f23b37147c513e3fb413
Tomas Apochian <tomasapochian@gmail.com>
2024-07-22 21:08:18 -0300

    fix receiver

--- ceibo-web/pages/api/contact.js
+++ ceibo-web/pages/api/contact.js
@@ -126,9 +126,9 @@ export default async (req, res) => {
 			case "evento":
 				({ name, lastname, email, empresa, cargo, subject, checked, phone } =
 					rest);
-				// receiver = "prensa@ceibo.digital";
+				receiver = "prensa@ceibo.digital";
 				oc = "Prensa & Eventos";
-				receiver = "tomas.apochian@ceibo.digital";
+				// receiver = "tomas.apochian@ceibo.digital";
 				data = {
 					to: receiver,
 					from: email,
